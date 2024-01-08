2b2a6798a94a6f40bdb300e5137cb59104148364
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-01-08 16:16:51 +0500

    admin-panel responsive finished

---
+++ admin panel/js/script.js
@@ -0,0 +1,9 @@
+// <show-side-bar 
+function showSidebar(){
+    var x = document.getElementById("sidebar")
+    if(x.style.display === "none"){
+        x.style.display = 'block'
+    }else{
+        x.style.display = 'none'
+    }
+} 
